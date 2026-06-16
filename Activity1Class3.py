02dfa0721ebca23f0fe063e53a1ce15d1c7f4432
RIFNALDO <rumana3712@gmail.com>
2026-06-16 08:59:10 -0400

    Add files via upload

---
+++ Activity1Class3.py
@@ -0,0 +1,11 @@
+a = 7
+print("The type of a is:", type(a))
+ 
+b = 6.7
+print("The type of b is:", type(b))
+
+c = True
+print("The type of c is: ", type(c))
+
+d = "Raaif"
+print("The type of d is:",type(d))
